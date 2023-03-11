7147b34b37ecf28e333f207dc64c14d688e618cd
amirsouri01 <amirsouri01@gmail.com>
2023-03-11 13:29:08 -0500

    Terraform main file

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 resource "aws_instance" "terEC2" {
   ami           = "ami-005f9685cb30f234b"
   instance_type = "t2.micro"
+  subnet_id     = "subnet-0de8f480df716b666"
 
   tags = {
+    Name      = "terraform-test"
     Createdby = "Terraform"
   }
 }
--- provider.tf
+++ provider.tf
@@ -2,15 +2,21 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.58.0"
+      version = "~> 4.0"
     }
   }
 }
 
 # Configure the AWS Provider
 
-  provider "aws" {
-  region     = "us-east-1"
-   
- }
- 
+provider "aws" {
+
+  region = "us-east-1"
+
+}
+
+resource "aws_vpc" "vpc-01" {
+
+  cidr_block = "10.10.0.0/24"
+
+}
