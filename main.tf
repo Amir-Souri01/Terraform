32027e243393ce2273ba111fb7c299ded237a42a
amirsouri01 <amirsouri01@gmail.com>
2023-03-12 13:57:43 -0400

    main file v7

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "terEC2" {
-  ami           = "ami-005f9685cb30f234b"
+  ami           = var.ami_id
   instance_type = "t2.micro"
   subnet_id     = "subnet-0de8f480df716b666"
 
