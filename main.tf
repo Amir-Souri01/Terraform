dab8d34a28e985d8e1789d630f011255ae067ac8
amirsouri01 <amirsouri01@gmail.com>
2023-03-12 11:45:04 -0400

    main file, adding key pairs

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "terEC2" {
   instance_type = "t2.micro"
   subnet_id     = "subnet-0de8f480df716b666"
   key_name      = "Key-pair"
+
   tags = {
     Name      = "terraform-test"
     Createdby = "Terraform"
