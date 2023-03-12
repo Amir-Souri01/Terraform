fee715da6fc5a7f62530a317fa6f59c5f917df7f
amirsouri01 <amirsouri01@gmail.com>
2023-03-12 14:10:17 -0400

    Variables file

--- variables.tf
+++ variables.tf
@@ -1,2 +1,7 @@
 variable "ami_id" { default = "ami-005f9685cb30f234b" }
 
+variable "instance_type" {default = "t2.micro"}
+
+variable "key_name" {default = "Key_name"}
+
+variable "machine_name" {default = "EC2-Terraform"}
