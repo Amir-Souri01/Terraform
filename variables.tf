e7c91a6819715a7d6d359b50e0bdf35f83d1c059
amirsouri01 <amirsouri01@gmail.com>
2023-03-12 14:49:47 -0400

    variables file

--- variables.tf
+++ variables.tf
@@ -1,7 +1,9 @@
 variable "ami_id" { default = "ami-005f9685cb30f234b" }
 
-variable "instance_type" {default = "t2.micro"}
+variable "instance_type" { default = "t2.micro" }
 
-variable "key_name" {default = "Key_name"}
+variable "key_name" { default = "Key_name" }
 
-variable "machine_name" {default = "EC2-Terraform"}
+variable "machine_name" { default = "EC2-Terraform" }
+
+variable "subnet_id" { default = "subnet-0de8f480df716b666"}
