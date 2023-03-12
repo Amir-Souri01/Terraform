ace6cec383b34004288e6e672c38398f6ae6d752
amirsouri01 <amirsouri01@gmail.com>
2023-03-12 10:56:09 -0400

    adding keys

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "terEC2" {
   ami           = "ami-005f9685cb30f234b"
   instance_type = "t2.micro"
   subnet_id     = "subnet-0de8f480df716b666"
-
+  key_name      = "Key-pair"
   tags = {
     Name      = "terraform-test"
     Createdby = "Terraform"
